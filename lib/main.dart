4b321120c5360a3893260b5c9f8812ccdc03c41a
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-23 22:05:38 +0300

    finish app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:recpiapp/screens/home_screem.dart';
 
 void main() {
-  runApp(const MyApp());
+ runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +18,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     home: HomeScreen(),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+     home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screem.dart
+++ lib/screens/home_screem.dart
@@ -6,7 +6,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xffE23E3E),
+                  Color(0xffFF6868),
+                  Color(0xffFF8383),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )),
+        ),
+        title: const Text(
+          'Recipe App',
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
     );
   }
 }
